cc148da1ab589889c5728b118e15ec8349e931f3
Alessio Crippa <alessio.crippa@gmail.com>
2016-10-26 13:44:47 +0200

    including time serie for visit

--- server.R
+++ server.R
@@ -5,9 +5,6 @@ library(dygraphs)
 library(xts)
 library(plotly)
 
-
-load("www/QRDF_shiny.Rdata")
-
 shinyServer(function(input, output){
    
    ## for subdiagnosis
@@ -21,8 +18,18 @@ shinyServer(function(input, output){
                         choices = choices, multiple = TRUE, selected = choices)
       }
    })
+   output$diagnos_1_besok <- renderUI({
+      if (is.null(input$diagnos_besok)){
+         return(NULL)
+      } else {
+         if (input$diagnos_besok == "All") return(NULL)
+         choices = as.character(unique(besok_basdata$diagnos_1[besok_basdata$diagnos_kategori1 == input$diagnos_besok]))
+         selectizeInput("sub_diag_besok", label = "Sub diagnosis",
+                        choices = choices, multiple = TRUE, selected = choices)
+      }
+   })
    output$diagnos_1_bio <- renderUI({
-      if (is.null(input$diagnos)){
+      if (is.null(input$diagnos_bio)){
          return(NULL)
       } else {
          if (input$diagnos_bio == "All") return(NULL)
@@ -33,15 +40,16 @@ shinyServer(function(input, output){
    })
    
    n_ts <- reactive({
-      group_bylist <- if (input$comp_region){
-         c(input$time_unit, "region")
+      group_bylist <- if (input$compare != "none"){
+         c(input$time_unit, input$compare)
       } else {
          c(input$time_unit)
       }
 
       basdata %>%
          ## exclude years before 1999 + interactive filtering
-         filter(year(inkluderad) >= 1999,
+         filter(#year(inkluderad) >= 1999,
+            inkluderad >= input$drange[1] & inkluderad <= input$drange[2],
                 (input$diagnos == "All" | (diagnos_kategori1 %in% input$diagnos &
                                            diagnos_1 %in% input$sub_diag))) %>%
          ## it's maybe better to create this variable in global.R
@@ -51,10 +59,25 @@ shinyServer(function(input, output){
          summarize(number = n())
    })
    
+   n_ts_besok <- reactive({
+      besok_basdata %>%
+         ## exclude years before 1999 + interactive filtering
+         filter(#year(inkluderad) >= 1999,
+                inkluderad >= input$drange_besok[1] & inkluderad <= input$drange_besok[2],
+                (input$diagnos_besok == "All" | (diagnos_kategori1 %in% input$diagnos_besok &
+                                              diagnos_1 %in% input$sub_diag_besok))) %>%
+         ## it's maybe better to create this variable in global.R
+         mutate(year = floor_date(inkluderad, "year"),
+                month = floor_date(inkluderad, "month")) %>%
+         group_by_(input$time_unit_besok) %>%
+         summarize(number = n())
+   })
+   
    n_ts_bio <- reactive({
       n_ts_bio <- terapi_basdata %>%
          ## exclude years before 1999 + interactive filtering
-         filter(ar >= 1999,
+         filter(#ar >= 1999,
+                inkluderad >= input$drange_bio[1] & inkluderad <= input$drange_bio[2],
                 (input$diagnos_bio == "All" | (diagnos_kategori1 %in% input$diagnos_bio &
                                                   diagnos_1.y %in% input$sub_diag_bio)),
                 (input$ongoing == FALSE | pagaende == 1)) %>%
@@ -77,6 +100,9 @@ shinyServer(function(input, output){
    output$table <- renderDataTable({
       n_ts()
    })
+   output$table_besok <- renderDataTable({
+      n_ts_besok()
+   })
    output$table_bio <- renderDataTable({
       n_ts_bio()
    })
@@ -93,16 +119,20 @@ shinyServer(function(input, output){
       #    ggplot(aes_string(x = input$time_unit, y = "number", color = color)) +
       #    geom_line() + theme_bw() + ylab("Number of patients")
      
-     dataxts <- if (input$comp_region){
-       nts <- spread(n_ts(), region, number)
+     dataxts <- if (input$compare != "none"){
+       nts <- n_ts() %>% spread_(input$compare, "number")
        as.xts(nts, order.by = nts[[input$time_unit]])
      } else {
        as.xts(n_ts(), order.by = n_ts()[[input$time_unit]])
      }
      dygraph(dataxts) %>% dyLegend(width = 700)
-     
    })
-   
+
+   output$tsplot_besok <- renderDygraph({
+      dataxts <- as.xts(n_ts_besok(), order.by = n_ts_besok()[[input$time_unit_besok]])
+      dygraph(dataxts) %>% dyLegend(width = 700)
+   })
+      
    output$tsplot_bio <- renderDygraph({
       dataxts <- if (input$biologic == "All"){
          as.xts(n_ts_bio()[, "total"], order.by = n_ts_bio()[[input$time_unit_bio]])
--- ui.R
+++ ui.R
@@ -1,9 +1,7 @@
 library(shiny)
 library(plotly)
 library(dygraphs)
-
-
-load("www/QRDF_shiny.Rdata")
+library(lubridate)
 
 shinyUI(
   navbarPage(
@@ -18,11 +16,15 @@ shinyUI(
                         choices = list("month", "year"), 
                         selected = "month"),
             h2("Subset"), 
+            sliderInput("drange", "Range limit", min = as.Date("1999-01-01") , max = max(basdata$inkluderad, na.rm = T),
+                        value = c(as.Date("1999-01-01"), max(basdata$inkluderad, na.rm = T))),
             selectInput("diagnos", label = "Diagnos",
                            choices = c("All", levels(basdata$diagnos_kategori1)), multiple = FALSE,
                            selected = NULL),
             uiOutput("diagnos_1"),
-            checkboxInput("comp_region", "Compare regions", FALSE)
+            selectInput("compare", label = "Comparison by",
+                        choices = list("none", "region", "kon", "tidig_ra"),
+                        selected = "none")
          ),
          
          mainPanel(
@@ -42,8 +44,31 @@ shinyUI(
   
    
    tabPanel(
-     "Visits"
-   ),
+     "Visits",
+     
+     sidebarLayout(
+        sidebarPanel(
+           selectInput("time_unit_besok", label = "Time unit",
+                       choices = list("month", "year"),
+                       selected = "month"),
+           h2("Subset"), 
+           sliderInput("drange_besok", "Range limit", min = as.Date("1999-01-01") , max = max(basdata$inkluderad, na.rm = T),
+                       value = c(as.Date("1999-01-01"), max(basdata$inkluderad, na.rm = T))),
+           selectInput("diagnos_besok", label = "Diagnos",
+                       choices = c("All", levels(besok_basdata$diagnos_kategori1)), multiple = FALSE,
+                       selected = NULL),
+           uiOutput("diagnos_1_besok")
+        ),
+        
+        mainPanel(
+           tabsetPanel(type = "tabs",
+                       tabPanel("Plot", dygraphOutput("tsplot_besok")),
+                       tabPanel("Table", dataTableOutput("table_besok")),
+                       tabPanel("Summary",
+                                p("Some text"))
+           )
+        )
+     )),
    
    tabPanel(
      "Bio Compounds",
@@ -55,6 +80,9 @@ shinyUI(
            selectInput("time_unit_bio", label = "Time unit",
                        choices = list("month", "year"),
                        selected = "month"),
+           h2("Subset"), 
+           sliderInput("drange_bio", "Range limit", min = as.Date("1999-01-01") , max = max(basdata$inkluderad, na.rm = T),
+                       value = c(as.Date("1999-01-01"), max(basdata$inkluderad, na.rm = T))),
            radioButtons("ongoing", "On going",
                         c("All"= FALSE, "Only on going"= TRUE)
            ),
