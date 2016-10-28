d2e4257ae6cc25c8bba43a43f94cedf5394d49ad
Alessio Crippa <alessio.crippa@gmail.com>
2016-10-28 11:11:20 +0200

    fixing spaces

--- global.R
+++ global.R
@@ -16,14 +16,16 @@ terapi_basdata <- merge(basdata, terapi, by = "patientkod")
 besok_basdata <- merge(basdata, besoksdata, by = "patientkod")
 
 ## category definition for diagnoskod_1
-basdata$dxcat[basdata$diagnoskod_1%in%c("M05.3","M05.8L","M05.8M","M05.8N","M05.9","M05.9L","M05.9M","M05.9N",
-                                        "M06.0","M06.0L","M06.0M","M06.0N","M06.8L","M06.8M","M06.8N","M06.9",
-                                        "M12.3" )]<-"RA"
-basdata$dxcat[basdata$diagnoskod_1%in%c("M45.9")]<-"AS"
-basdata$dxcat[basdata$diagnoskod_1%in%c("M46.8","M46.9","M08.1")]<-"SPA"
-basdata$dxcat[basdata$diagnoskod_1%in%c("M07.0","M07.1","M07.2","M07.3x+L40.5","M09.0",
-                                        "M07.0+L40.5","M07.1+L40.5","M07.2+L40.5")]<-"PSA"
-
+basdata$dxcat[basdata$diagnoskod_1 %in% c("M05.3", "M05.8L", "M05.8M", "M05.8N",
+                                          "M05.9", "M05.9L", "M05.9M", "M05.9N",
+                                          "M06.0", "M06.0L", "M06.0M", "M06.0N",
+                                          "M06.8L", "M06.8M", "M06.8N", "M06.9",
+                                          "M12.3" )] <- "RA"
+basdata$dxcat[basdata$diagnoskod_1 %in% c("M45.9")] <- "AS"
+basdata$dxcat[basdata$diagnoskod_1 %in% c("M46.8","M46.9","M08.1")] <- "SPA"
+basdata$dxcat[basdata$diagnoskod_1 %in% c("M07.0", "M07.1", "M07.2", "M07.3x+L40.5", 
+                                          "M09.0", "M07.0+L40.5", "M07.1+L40.5", 
+                                          "M07.2+L40.5")] <- "PSA"
 
 ## creating variable for KM plot: 1) status; 2) time
 terapi_basdata$status <- as.numeric(!is.na(terapi_basdata$utsatt) & terapi_basdata$avslutad != terapi_basdata$utsatt)
