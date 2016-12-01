15b99c91abb7eead5472d4c0550f0e76be8a2b8f
Alessio Crippa <alessio.crippa@gmail.com>
2016-12-01 13:32:43 +0100

    changing age categories

--- global.R
+++ global.R
@@ -3,3 +3,8 @@ library(lubridate)
 library(survival)
 
 load("www/QRDF_shiny.Rdata")
+
+## new categorie for age
+basdata$age_inclusion_cat <- cut(basdata$age_inclusion, breaks =  c(18, 30, 45, 55, 65, 110), right = FALSE)
+besok_basdata$age_visit_cat <- cut(besok_basdata$age_visit, breaks =  c(18, 30, 45, 55, 65, 110), right = FALSE)
+terapi_basdata$age_ordinerat_cat <- cut(terapi_basdata$age_ordinerat, breaks =  c(18, 30, 45, 55, 65, 110), right = FALSE)
